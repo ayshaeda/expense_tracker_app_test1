17e7975dabc2a2477d1654e2f6037d5338dcf69d
ayshaeda <aysha.edathodu@gmail.com>
2025-09-25 23:54:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/expense_tracker_app_test1/",
+  base: "/",
   build: {
-    outDir: "dist",
+    outDir: "dist", // output folder for build
   },
   server: {
-    port: 3000,
-    open: true,
+    port: 3000, // optional for local dev
+    open: true,  // opens browser on npm run dev
   },
 })
